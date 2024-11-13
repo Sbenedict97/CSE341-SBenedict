9ff16cba0c370e9a9c2480d2878fbea8e6b41935
Sbenedict97 <ben21002@byui.edu>
2024-11-12 19:35:17 -0700

    W02 Personal Assignment

--- routes/contacts.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/users");
-
-router.get("/", userController.getAllUsers);
-
-router.get("/:id", userController.getUserById);
-
-module.exports = router;
